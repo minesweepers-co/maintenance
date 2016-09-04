d122798cec53c4cf4c8afdbff3e07f72223f2299
Harsh <shahmharsh@gmail.com>
2016-09-04 13:35:42 -0700

    Firebase Authentication

--- app/src/main/java/co/minesweepers/maintenance/LoginActivity.java
+++ app/src/main/java/co/minesweepers/maintenance/LoginActivity.java
@@ -3,6 +3,7 @@ package co.minesweepers.maintenance;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,94 +11,135 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
-        private static final String TAG = "LoginActivity";
-        private static final int REQUEST_LOGIN = 0;
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_LOGIN = 0;
 
-        @Bind(R.id.input_email) EditText mEmailText;
-        @Bind(R.id.input_password) EditText mPasswordText;
-        @Bind(R.id.btn_login) Button mLoginButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
-            ButterKnife.bind(this);
+    @Bind(R.id.input_email) EditText mEmailText;
+    @Bind(R.id.input_password) EditText mPasswordText;
+    @Bind(R.id.btn_login) Button mLoginButton;
 
-            mLoginButton.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    login();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivityForResult(intent, REQUEST_LOGIN);
+                    finish();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    onLoginFailed();
                 }
-            });
-        }
-
-        public void login() {
-            Log.d(TAG, "Login");
-
-            if (!validate()) {
-                onLoginFailed();
-                return;
             }
+        };
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
 
-            mLoginButton.setEnabled(false);
-
-            final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-            progressDialog.setIndeterminate(true);
-            progressDialog.setMessage("Authenticating...");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-
-            final String email = mEmailText.getText().toString();
-            final String password = mPasswordText.getText().toString();
-
-            new android.os.Handler().post(
-                    new Runnable() {
-                        public void run() {
-                            // On complete call either onLoginSuccess or onLoginFailed
-                            progressDialog.dismiss();
-                            if(email.equalsIgnoreCase("a@test.com") && password.equalsIgnoreCase("password")) {
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                startActivityForResult(intent, REQUEST_LOGIN);
-                                finish();
-                            } else {
-                                onLoginFailed();
-                            }
-                        }
-                    });
-        }
-
-        public void onLoginFailed() {
-            Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-            mLoginButton.setEnabled(true);
-        }
-
-        public boolean validate() {
-            boolean valid = true;
-
-            String email = mEmailText.getText().toString();
-            String password = mPasswordText.getText().toString();
-
-            if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                mEmailText.setError("enter a valid email address");
-                valid = false;
-            } else {
-                mEmailText.setError(null);
+            @Override
+            public void onClick(View v) {
+                login();
             }
+        });
+    }
 
-            if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-                mPasswordText.setError("between 4 and 10 alphanumeric characters");
-                valid = false;
-            } else {
-                mPasswordText.setError(null);
-            }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
-            return valid;
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        mLoginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        final String email = mEmailText.getText().toString();
+        final String password = mPasswordText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            onLoginFailed();
+                        }
+                    }
+                });
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
+        mLoginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            mEmailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            mPasswordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            mPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
 
